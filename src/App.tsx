9aed036514f77577cb6b5eb541fafe1252efc2d2
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2024-01-12 00:19:40 -0300

    chore: implement shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export function App() {
   return (
-    <h1>hello world</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button variant="ghost">Enviar</Button>
+    </div>
   )
 }
  
