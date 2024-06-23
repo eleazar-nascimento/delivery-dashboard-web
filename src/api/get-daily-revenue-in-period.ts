5d2987911d328bc887c72f2245d45968472aa8fa
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2024-06-23 14:05:45 -0300

    feat:  implement daily revenue in period metrics and tratactive data

--- src/api/get-daily-revenue-in-period.ts
+++ src/api/get-daily-revenue-in-period.ts
@@ -1,13 +1,27 @@
 import { api } from '@/lib/axios'
 
+export interface GetDailyRevenueInPeriodQuery {
+  from?: Date
+  to?: Date
+}
+
 export type GetDailyRevenueInPeriodResponse = {
-  datge: string
+  date: string
   receipt: number
 }[]
 
-export async function getDailyRevenueInPeriod() {
+export async function getDailyRevenueInPeriod({
+  to,
+  from,
+}: GetDailyRevenueInPeriodQuery) {
   const response = await api.get<GetDailyRevenueInPeriodResponse>(
     '/metrics/daily-receipt-in-period',
+    {
+      params: {
+        to,
+        from,
+      },
+    },
   )
 
   return response.data
