f2afb5b1614c65a9fed68ad2b40312339eebb660
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2024-06-24 09:48:20 -0300

    feat:  implement loading in orders

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -5,6 +5,7 @@ import { z } from 'zod'
 
 import { getOrders } from '@/api/get-orders'
 import { Pagination } from '@/components/old-pagination'
+import { OrderTableSkeleton } from '@/components/skeletons/order-table-skeleton'
 import {
   Table,
   TableBody,
@@ -28,7 +29,7 @@ export function Orders() {
     .transform((page) => page - 1)
     .parse(searchParams.get('page') ?? '1')
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ['orders', pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -70,6 +71,8 @@ export function Orders() {
               </TableRow>
             </TableHeader>
             <TableBody>
+              {isLoadingOrders && <OrderTableSkeleton />}
+
               {result &&
                 result.orders.map((order) => {
                   return <OrderTableRow key={order.orderId} order={order} />
