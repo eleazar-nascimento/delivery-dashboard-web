ccf8bd34fa49cba38fb907181d9f60c4a0b8f1bf
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2024-08-12 22:40:39 -0300

    feat:  implement test setup environment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig, InlineConfig, UserConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
